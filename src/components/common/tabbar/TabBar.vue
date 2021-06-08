60bbb9c7f85e5183e255297b46783039e93d91f9
coderyang <991289144@qq.com>
2021-06-08 18:08:34 +0800

    导入tabbar

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="tab-bar" >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBar"
+  }
+</script>
+
+<style scoped>
+  #tab-bar{
+    display: flex;
+    background-color: #f1f1f1;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    box-shadow: 0 -2px 10px rgba(100,100,100,.2);
+  }
+
+</style>
